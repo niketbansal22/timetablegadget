3ad49eba1116c6e4603d03f7ff412c4d2408aa2e
ilyabreev <ilyabreev@gmail.com>
2010-06-24 20:12:28 +0000

    Partially added russian locale

---
+++ ru-ru/locale.js
@@ -0,0 +1,46 @@
+var strings = {
+	// дни недели
+	monday: 'Понедельник',
+	tuesday: 'Вторник',
+	wednsday: 'Среда',
+	thursday: 'Четверг',
+	friday: 'Пятница',
+	saturday: 'Суббота',
+	sunday: 'Воскресенье',
+	
+	// сокращенные названия дней
+	mon: 'Пн',
+	tue: 'Вт',
+	wed: 'Ср',
+	thu: 'Чт',
+	fri: 'Пт',
+	sat: 'Сб',
+	sun: 'Вс',
+	
+	// месяцы
+	january: 'Январь',
+	february: 'Февраль',
+	march: 'Март',
+	april: 'Апрель',
+	may: 'Май',
+	june: 'Июнь',
+	jule: 'Июль',
+	august: 'Август',
+	september: 'Сентябрь',
+	october: 'Октябрь',
+	november: 'Ноябрь',
+	december: 'Декабрь',
+
+	// default-строки
+	dayOfTheWeek: 'День недели',
+	currentDate: 'Текущая дата',
+	parityOfTheWeek: 'Четность недели',
+	timetable: 'Расписание',
+	
+	// четность
+	even: 'Четная неделя',
+	odd: 'Нечетная неделя'
+	
+	// страница настроек
+	
+}
