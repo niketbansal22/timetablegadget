6eb477782e63d3d1f2087ec283b8cdab4e0bbef2
ilyabreev <ilyabreev@gmail.com>
2014-09-24 18:32:31 +0000

    Reverted to last stable version

--- uninstall.bat
+++
@@ -1,6 +0,0 @@
-cd "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Microsoft\Windows Sidebar"
-%HOMEDRIVE%
-rmdir /S /Q ".\Gadgets\timetable.gadget"
-find /V /I "timetable.gadget" < Settings.ini  > Settings.ini.temp
-copy Settings.ini.temp Settings.ini
-del Settings.ini.temp
